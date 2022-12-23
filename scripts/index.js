292bb270d00eafac7320623b2ec9d4c9d3583b81
lesha <oookosmos.m@yandex.ru>
2022-12-24 01:35:59 +0300

    Develop

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,33 @@
  
  const profileClosePopup = profilepopup.querySelector('.popup__close-popup');
  
-
- profilebuttun.addEventListener('click', () =>{
-
-    profilepopup.classList.add('popup_opened');
-
- } );
-
- profileClosePopup.addEventListener('click',() =>{
-    
-    profilepopup.classList.remove('popup_opened');
-});
-
 const buttonSave  = document.querySelector('.popup__button-save');
-const popupFieldName = document.querySelector('.popup__input_name');
+const inputName = document.querySelector('.popup__input_name');
 const profileTitle = document.querySelector('.profile__title');
-const fieldDescription = document.querySelector('.popup__input_description');
+const inputDescription = document.querySelector('.popup__input_description');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+
+
+function openPopup(){
+   profilepopup.classList.add('popup_opened')
+   inputName.value = profileTitle.textContent;
+   inputDescription.value = profileSubtitle.textContent;
+};
+
+function closePopup(){
+   profilepopup.classList.remove('popup_opened')
+};
+
+   profilebuttun.addEventListener('click',openPopup);
+   profileClosePopup.addEventListener('click', closePopup);
+ 
+
 
 function save(evt) {
    evt.preventDefault();
-   profilepopup.classList.add(profileClosePopup);
-   profileTitle.textContent = popupFieldName.value;
-   profileSubtitle.textContent = fieldDescription.value;
+   
+   profileTitle.textContent = inputName.value;
+   profileSubtitle.textContent = inputDescription.value;
+   closePopup();
 };
-buttonSave.addEventListener('click', save);
+buttonSave.addEventListener('click' /*еслси суда ставлю submit не сохраняет*/, save);
